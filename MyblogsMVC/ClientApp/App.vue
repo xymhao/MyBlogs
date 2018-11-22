338e03e3dfdb1d505fffb2d078996e7f53a1d315
yueming xu <xnysym@outlook.com>
2018-11-22 22:35:43 +0800

    初始化

---
+++ MyblogsMVC/ClientApp/App.vue
@@ -0,0 +1,10 @@
+﻿<template>
+    <div id="home">
+        <h1>Hello World!</h1>
+        <h1>I an what I an</h1>
+    </div>
+</template>
+<script>export default { }</script>
+<style lang="scss">
+
+</style>
---
+++ MyblogsMVC/ClientApp/index.js
@@ -0,0 +1,24 @@
+﻿import Vue from 'vue'
+import VueRouter from 'vue-router'  //导入路由插件的包
+import App from './App.vue'  //导入根组件
+
+Vue.config.productionTip = false
+Vue.use(VueRouter) //安装路由模块
+
+const routes = [
+    {
+        path: '/',
+        component: App
+    }
+]
+
+const router = new VueRouter({    //创建路由对象
+    routes,
+    mode: 'history'
+})
+
+new Vue({
+    el: '#app',
+    render: h => h(App),
+    router //挂载路由对象到 VM 实例上
+})

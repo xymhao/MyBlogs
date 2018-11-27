12824c4c0a319337be4c228e057a128a7e08740f
许玥明 <xnysym@icloud.com>
2018-11-27 15:41:45 +0800

    angular配置

--- AngularWeb/ClientApp/src/app/app.module.ts
+++ AngularWeb/ClientApp/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ElModule } from 'element-angular'
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -22,6 +25,8 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    ElModule.forRoot(),
+    BrowserAnimationsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
--- AngularWeb/ClientApp/src/app/counter/counter.component.ts
+++ AngularWeb/ClientApp/src/app/counter/counter.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-counter-component',
   templateUrl: './counter.component.html'
@@ -10,4 +11,26 @@ export class CounterComponent {
   public incrementCounter() {
     this.currentCount++;
   }
+  public tableData: any[] = [{
+    name: '水爷',
+    date: '2017-08-19',
+    address: '上海市普陀区金沙江路 1518 弄',
+  }, {
+    name: '水爷',
+    date: '2017-08-20',
+    address: '上海市普陀区金沙江路 1518 弄',
+  }, {
+    name: '水爷',
+    date: '2017-08-21',
+    address: '上海市普陀区金沙江路 1518 弄',
+  }, {
+    name: '水爷',
+    date: '2017-08-22',
+    address: '上海市普陀区金沙江路 1510 弄',
+    }];
+
+  public handle(cope) {
+    this.tableData.shift();
+  }
+
 }
